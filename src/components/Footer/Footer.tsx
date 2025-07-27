a0ede7037c9eaab9e69d2e23b3677568ba09b877
vlad <vladm1225@yandex.ru>
2025-07-27 10:53:16 +0400

    add  cv

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Social } from "../Social/Social";
 
 export const Footer = () => {
   const socials = [
-    // { title: "cv", link: "" },
+    { title: "cv", link: "./cv.pdf" },
     { title: "telegram", link: "https://t.me/jr_matviyuk" },
     { title: "github", link: "https://github.com/VladMatviyuk" },
     { title: "codepen", link: "https://codepen.io/VladMatviyuk" },
